c25a66891dbf14a7e4b1f40b8a2cd5ce3f197c99
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2020-12-19 15:56:15 +0300

    separated

--- src/pages/SignUp.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SignUp = () => {
-  return <div></div>
-}
-
-export default SignUp
--- src/redux/root-saga.js
+++
@@ -1,7 +0,0 @@
-import { all, call } from 'redux-saga/effects'
-
-function* rootSaga() {
-  // yield all([call(productsSagas)])
-}
-
-export default rootSaga
