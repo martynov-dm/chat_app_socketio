9c70d722f841516e83949f5823d525ce3a85ed55
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2020-12-19 20:48:02 +0300

    added default styles, set up sc theme, added ts

---
+++ client/src/theme/theme.ts
@@ -0,0 +1,14 @@
+const myTheme = {
+  borderRadius: '5px',
+  colors: {
+    primary: '#252329',
+    primaryDark1: '#120F13',
+    primaryDark2: '#0B090C',
+    textLight1: '#E0E0E0',
+    textLight2: '#BDBDBD',
+    textDark: '#828282',
+    secondary: '#2F80ED',
+  },
+}
+
+export default myTheme
